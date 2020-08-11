229cedf0f5af415ce2d923a4ae8cd39ff87895a0
shihab <echoshihab@gmail.com>
2020-08-10 20:57:28 -0400

    add reverse method to custom linked list

--- DataStructures/LinkedLists/CustomLinkedList.js
+++ DataStructures/LinkedLists/CustomLinkedList.js
@@ -67,6 +67,22 @@ class LinkedList {
     return currentNode;
   }
 
+  reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+  }
+
   remove(index) {
     let leader;
     if (index === 0) {
