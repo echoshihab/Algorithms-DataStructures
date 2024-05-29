113d9f83a87af6c0af9c51d3b971e906c47d7e9b
Shihab Khan <echoshihab@gmail.com>
2024-05-28 22:46:08 -0400

    add alternate solution to detecting anagram using hash map

--- Problems/validAnagram.py
+++ Problems/validAnagram.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 """Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
@@ -22,7 +24,7 @@ s and t consist of lowercase English letters.
 
 Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?"""
 
-#brute force
+#O(n) 
 def isAnagram(s: str, t: str) -> bool:
     if len(s) != len(t):
         return False
@@ -49,5 +51,28 @@ def isAnagram(s: str, t: str) -> bool:
 
 print(isAnagram("rat", "car"))
 print(isAnagram("anagram", "nagaram"))
+
+
+# alternative
+def isAnagramAlt(s: str, t: str) -> bool:
+
+    charCountDict = defaultdict(int)
+
+    for letter in s:
+        charCountDict[letter] += 1
+
+    for letter in t:
+        charCountDict[letter] -= 1
+
+    for val in charCountDict.values():
+        if val != 0 :
+            return False
+        
+    return True
+
+    
+print(isAnagramAlt("rat", "car"))
+print(isAnagramAlt("anagram", "nagaram"))
+
 
         
