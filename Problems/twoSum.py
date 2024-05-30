ed123563c859d624e409bcac13cd52c99e12f43a
Shihab Khan <echoshihab@gmail.com>
2024-05-29 22:14:37 -0400

    find target sum in an array in o(n) and o(n^2) run time

---
+++ Problems/twoSum.py
@@ -0,0 +1,66 @@
+
+from typing import List
+
+"""Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists."""
+
+[1,2,3,4]
+#brute force O(n^2)
+def twoSum(nums: List[int], target: int) -> List[int]:
+    for i in range(0, len(nums) - 1):
+        selectedNum = nums[i]
+        for j in range (i+1, len(nums)):
+            if (selectedNum + nums[j]) == target:
+                return [i,j]
+    return False
+                
+
+print(twoSum([2,7,11,15], 9))
+print(twoSum([3,2,4], 6))
+print(twoSum([3,3], 6))
+
+#o(n)
+def twoSumDict(nums: List[int], target: int) -> List[int]:
+    check_dict = {}
+
+    for i in range(0, len(nums)):
+        target_num = target - nums[i]
+        if check_dict.get(target_num) is not None:
+            return [check_dict[target_num], i]
+        check_dict[nums[i]]  = i
+    return False
+
+print(twoSumDict([2,7,11,15], 9))
+print(twoSumDict([3,2,4], 6))
+print(twoSumDict([3,3], 6))
+
+
+
+        
