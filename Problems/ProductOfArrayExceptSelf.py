c09569db1768454e3f3e09d0df2d2d9dd33ad079
Shihab Khan <echoshihab@gmail.com>
2024-06-09 20:33:53 -0400

    improve space time complexity by using the result array to store the prefix and postfix value

--- Problems/ProductOfArrayExceptSelf.py
+++ Problems/ProductOfArrayExceptSelf.py
@@ -61,4 +61,21 @@ print(productExceptSelf([1,2,3,4]))
             
 
 
-        
+def productExceptSelf2(nums: List[int]) -> List[int]:
+    result = [1] * (len(nums))
+
+    prefix = 1
+    postfix = 1
+
+    for i in range(len(nums)):
+        result[i] = prefix  
+        prefix *= nums[i]
+    
+    for i in range(len(nums) -1, -1, -1):
+        result[i] *= postfix
+        postfix *= nums[i]
+    
+    return result
+
+print(productExceptSelf2([1,2,3,4]))
+
