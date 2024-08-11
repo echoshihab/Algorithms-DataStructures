47d55c578c123b66ed55dbce62663ee76f64a53e
Shihab Khan <echoshihab@gmail.com>
2024-08-11 16:36:35 -0400

    add O(n) time complexity and O(1) space complexity solution for calculating elevation map

--- Problems/TrappingRainWater.py
+++ Problems/TrappingRainWater.py
@@ -39,6 +39,33 @@ def trap(height: List[int]) -> int:
 
 
 
+#o(n) runtime o(1) space 
+def trap2(height: List[int]) -> int:
+    if not height:
+        return 0
+    
+    left_pointer, right_pointer = 0, len(height)-1
+    left_max, right_max = height[left_pointer], height[right_pointer]
+    water_collected = 0
+
+    while left_pointer < right_pointer:
+        if left_max < right_max:
+            left_pointer += 1
+            left_max = max(left_max, height[left_pointer])
+            water_collected += left_max - height[left_pointer]
+        else:
+            right_pointer -=1 
+            right_max = max(right_max, height[right_pointer])
+            water_collected += right_max - height[right_pointer]
+
+    return water_collected
+
+
+print(trap2([0,1,0,2,1,0,1,3,2,1,2,1]))
+print(trap2([4,2,0,3,2,5]))
+print(trap2([4,2,3]))
+
+
 print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
 print(trap([4,2,0,3,2,5]))
 print(trap([4,2,3]))
