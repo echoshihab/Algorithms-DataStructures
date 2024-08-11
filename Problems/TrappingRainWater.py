45bb36ee99de563b15595f67f5489a28d3110cde
Shihab Khan <echoshihab@gmail.com>
2024-08-11 16:34:58 -0400

    add o(n) space and time complexity solution for trapping rain water problem

--- Problems/TrappingRainWater.py
+++ Problems/TrappingRainWater.py
@@ -1,35 +1,44 @@
 """Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
 """
+from collections import defaultdict
 from typing import List
 
 
 def trap(height: List[int]) -> int:
-    water_collected = 0
-    water_collected_confirmed = 0
-    left_index = 0 
-    peak = 0
-    peak_set = False
+    
+    total_water_collected = 0
+
+    if not height:
+        return 0
+    
+    max_left, max_right = 0, 0
+    max_left_arr = [max_left]
+    max_right_arr = [max_right]
+
+    for h in range(1, len(height)):
+        if height[h - 1] >= max_left:
+           max_left = height[h -1]
+        max_left_arr.append(max_left)
+
+    for h in range(len(height) -2, -1, -1):
+        if height[h + 1] >= max_right:
+            max_right = height[h + 1]
+        max_right_arr.insert(0, max_right)
 
 
-    while left_index < len(height):
-        print(height[left_index])
-        if height[left_index] > peak and peak_set == False:
-            peak = height[left_index]
-            print("setting peak with ", height[left_index])
-            peak_set = True
-        elif height[left_index] >= peak and peak_set == True:
-            print("reached second peak with ")
-            water_collected_confirmed += water_collected
+    
+    for i in range(len(height)):
+        water_collected = min(max_left_arr[i], max_right_arr[i]) - height[i]
+        if water_collected < 0:
             water_collected = 0
-            peak = height[left_index]
-        elif height[left_index] < peak:
-            water_collected += (peak - height[left_index])
-        
-        left_index += 1
+        total_water_collected += water_collected
 
-    return water_collected_confirmed
+    return total_water_collected     
+
 
 
 
 print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+print(trap([4,2,0,3,2,5]))
+print(trap([4,2,3]))
