f18f532747437dda7c40dc5062c8351eedd089b2
Shihab Khan <echoshihab@gmail.com>
2024-08-17 15:26:22 -0400

    clean up solution  for minstack problem

--- Problems/minStack.py
+++ Problems/minStack.py
@@ -21,21 +21,21 @@ class MinStack:
     def push(self, val: int) -> None:
         self.stack.append(val)
 
-        if len(self.min) == 0 or self.min[len(self.min) - 1] >= val:
+        if len(self.min) == 0 or self.min[-1] >= val:
             self.min.append(val)
 
     def pop(self) -> None:
         item = self.stack.pop()
-        if (item == self.min[len(self.min) - 1]):
+        if (item == self.min[-1]):
             self.min.pop()
 
     
     def top(self):
-        return self.stack[len(self.stack) - 1]
+        return self.stack[-1]
 
 
     def getMin(self) -> int:
-        return self.min[len(self.min) - 1]
+        return self.min[-1]
     
     def print(self):
         print(self.stack)
