58dd4c755379e900ffecbe9cb682b0c68adba263
Shihab Khan <echoshihab@gmail.com>
2025-06-05 21:40:58 -0400

    code cleanup for last stone weight solution

--- Problems/lastStoneWeight.py
+++ Problems/lastStoneWeight.py
@@ -22,18 +22,16 @@ from typing import List
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        max_heap = []
-        for item in stones:
-            heapq.heappush(max_heap, -item)
+        max_heap = [-stone for stone in stones]
+        heapq.heapify(max_heap)
 
         while len(max_heap) > 1:
-            x = -(heapq.heappop(max_heap))
-            y = -(heapq.heappop(max_heap))
+            x = heapq.heappop(max_heap)
+            y = heapq.heappop(max_heap)
 
-            if x == y:
-                continue
-        
-            heapq.heappush(max_heap, -(x-y))
+            if y > x:
+                heapq.heappush(max_heap, x - y)
 
-        return -max_heap[0] if len(max_heap) else 0
+        max_heap.append(0)
+        return abs(max_heap[0])
 
