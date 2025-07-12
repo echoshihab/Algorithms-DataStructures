618541989a072f94683711e420720128e9d4c582
Shihab Khan <echoshihab@gmail.com>
2025-07-12 15:23:20 -0400

    identify integer palindrome by comparing halves of each number

--- Problems/isPalindromeNumber.py
+++ Problems/isPalindromeNumber.py
@@ -20,4 +20,17 @@ class Solution:
             original //= 10
         
         return x == reversed_num
+
+
+class Solution2:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x > 0 and x % 10 == 0): return False
+
+        reversed_num = 0
+
+        while x > reversed_num:
+            reversed_num = reversed_num * 10 + (x % 10)
+            x //= 10
+        
+        return x == reversed_num or x == reversed_num // 10
             
