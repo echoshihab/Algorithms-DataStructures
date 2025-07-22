252e58885fd29f4f27cd58f8fc00628d289eb213
Shihab Khan <echoshihab@gmail.com>
2025-07-21 20:26:57 -0400

    solve for number of islands given a grid of 0s and 1s using dfs in O(m x n) time complexity

--- Problems/numberOfIslands.py
+++ Problems/numberOfIslands.py
@@ -69,3 +69,35 @@ class Solution:
                     islandsCount += 1
 
         return islandsCount
+    
+
+
+
+class Solution2:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islandsCount = 0
+
+        if not grid:
+            islandsCount
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] != '1':
+                return
+            grid[r][c] = '0'
+            dfs(r+1 , c)
+            dfs(r-1 , c)
+            dfs(r , c + 1)
+            dfs(r , c -1)
+
+
+        
+        rows, columns = len(grid), len(grid[0])
+        
+
+        for row in range(rows):
+            for column in range(columns):
+                if grid[row][column] == "1":
+                    dfs(row, column)
+                    islandsCount += 1
+
+        return islandsCount    
