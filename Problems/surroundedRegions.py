ba5bb1c897bc6ecc0f1022a0d2839b7052d4568e
Shihab Khan <echoshihab@gmail.com>
2025-09-02 22:23:43 -0400

    add bfs solution to capturing regions that are surrounded

--- Problems/surroundedRegions.py
+++ Problems/surroundedRegions.py
@@ -7,6 +7,7 @@ Surround: The region is surrounded with 'X' cells if you can connect the region 
 To capture a surrounded region, replace all 'O's with 'X's in-place within the original board. You do not need to return anything.
 """
 
+import collections
 from typing import List
 
 #O(n*m) using extra space for visited
@@ -80,6 +81,54 @@ class Solution2:
         for i in range(ROWS):            
             dfs(i, 0) #left
             dfs(i, COLS-1) #right
+
+        
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'T':
+                    board[i][j] = 'O'
+
+
+
+#bfs
+class Solution3:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        # go through edge of the board, find all 0s
+        # when found find all connected 0s
+        # make sure that we keep track of the found 0s
+
+        ROWS, COLS = len(board), len(board[0])        
+
+        def bfs(r, c):
+
+
+            queue = collections.deque()
+            queue.append((r,c))
+            
+            directions = [(0,1), (0, -1), (1, 0), (-1,0)]
+
+            while queue:
+                r, c = queue.popleft()
+                if board[r][c] == 'O':
+                    board[r][c] = 'T'
+                    for dr,dc in directions:
+                        nr, nc =  r + dr, c + dc
+                        if nr < 0 or nc < 0 or nr == ROWS or nc == COLS or board[nr][nc] != 'O':
+                            continue
+                        queue.append((nr, nc))
+            
+        for i in range(COLS):            
+            bfs(0, i) #top
+            bfs(ROWS-1, i) #bottom      
+            
+        for i in range(ROWS):            
+            bfs(i, 0) #left
+            bfs(i, COLS-1) #right
 
         
         for i in range(ROWS):
