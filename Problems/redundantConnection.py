5d47760a230d52635d2361f6a44b90b1d8f84eb2
Shihab Khan <echoshihab@gmail.com>
2025-09-25 23:35:32 -0400

    find redundant connection in a graph using union find

---
+++ Problems/redundantConnection.py
@@ -0,0 +1,44 @@
+"""
+In this problem, a tree is an undirected graph that is connected and has no cycles.
+
+You are given a graph that started as a tree with n nodes labeled from 1 to n, 
+with one additional edge added. The added edge has two different vertices chosen from 1 to n, 
+and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] 
+indicates that there is an edge between nodes ai and bi in the graph.
+
+Return an edge that can be removed so that the resulting graph is a tree of n nodes. 
+If there are multiple answers, return the answer that occurs last in the input.
+
+Constraints:
+
+n == edges.length
+3 <= n <= 1000
+edges[i].length == 2
+1 <= ai < bi <= edges.length
+ai != bi
+There are no repeated edges.
+The given graph is connected.
+"""
+
+from typing import List
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        root = list(range(len(edges) + 1))
+        print(root)
+
+        def find_root(node):
+            if root[node] != node:
+                root[node] = find_root(root[node]) 
+            return root[node] 
+        for node1, node2 in edges: 
+            root1, root2 = find_root(node1), find_root(node2)
+
+            if root1 == root2:
+                return [node1, node2] 
+ 
+            root[root2] = root1 
+    
+
+test = Solution()
+test.findRedundantConnection([[1,2],[1,3],[2,3]])
